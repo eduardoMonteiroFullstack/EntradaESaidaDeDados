5a3702c0c8b03712dd4172c2d1480e44fb3377d2
eduardo monteiro <eduardomonteirotecnologiadainf@gmail.com>
2025-03-02 14:55:04 -0300

    entrada e saída de dados

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("digite seu nome : ");
+        String name = sc.nextLine();
+        System.out.println(name);
+
+
+
+        sc.close();
+    }
+}
